b63d38d0d5d2c667132719529b8e0eefbe747349
etien-andres <andres.etien@gmail.com>
2015-10-18 17:36:57 -0500

    corregido

--- punteros/puntero.cpp
+++ punteros/puntero.cpp
@@ -1,17 +1,19 @@
 #include<iostream>
-#include<cstdlib>
 using namespace std;
 int main(){
-	int tam=0, areg[tam];
+	int i=0,tam=0,*areglo;
 	cout<<"De el tamano del arreglo: ";
 	cin>>tam;
-	int *pun=(int*)malloc(sizeof(int)*tam);
-	pun=areg;
-	for(int i=0;i<tam;i++){
-		*pun=i+1;
-		cout<<areg[i]<<", ";
-		pun++;
+	areglo=new int[tam];
+	for(i=0;i<tam;i++){
+		areglo[i]=i+1;
 	}
-	free(pun);
+	cout<<endl;
+	cout<<"El arreglo es \n";
+	for(i=0;i<tam;i++){
+		cout<<areglo[i]<<", ";
+	}
+	cout<<endl;
+	delete [] areglo;
 	return 0;
 }
