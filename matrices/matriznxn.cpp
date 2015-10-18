f628cdfbf961ca75d5b8369ddabd70c12f1a39c9
etien-andres <andres.etien@gmail.com>
2015-10-18 17:19:12 -0500

    corregir errores

--- matrices/matriznxn.cpp
+++ matrices/matriznxn.cpp
@@ -2,44 +2,44 @@
 
 using namespace std;
 
-void fill(float mat[][n], int n);
+void fill(float mat[][99], int n);
 
-void show(float mat[][n], int n);
+void show(float mat[][99], int n);
 
-void mult(float matA[][n],float matB[][n], int n);
+void mult(float matA[][99],float matB[][99], int n);
 
 int n=99;
-float mat[n][n], res[n][n];
+float mat[99][99], res[99][99];
 
 int main()
 {
-	
-	float matA[n][n], matB[n][n];
+	int x=9;
 	cout<<"De el tamaño de las matrices.. \n";
-	cin>>n;
+	cin>>x;
+	float matA[99][99], matB[99][99];
 	cout <<"Llene la primera matriz (nxn)\n";
-	fill(matA,n); 
+	fill(matA,x); 
 	cout <<"Llene la segunda matriz (nxn)\n";
-	fill(matB,n);
+	fill(matB,x);
 	cout<<"las matrices son: \n";
-	show(matA,n);
+	show(matA,x);
 	cout<<"\ny\n";
-	show(matB,n);
+	show(matB,x);
 	cout<<"la multiplicacion es: \n";
-	mult(matA,matB,n);
-	show(res);
+	mult(matA,matB,x);
+	show(res,x);
 	return 0;
 }
 
 
-void fill(float mat[][n], int n)
+void fill(float mat[][99], int n)
 {
 	int i=0,j=0;
 	for (i=0;i<n;i++)
 		for (j=0;j<n;j++)
 			cin>>mat[i][j];
 }
-void show(float mat[][n], int n)
+void show(float mat[][99], int n)
 {
 	int i=0,j=0;
 	for(i=0;i<n;i++)
@@ -51,7 +51,7 @@ void show(float mat[][n], int n)
 	}
 }
 
-void mult (float matA[][n],float matB[][n], int n)
+void mult (float matA[][99],float matB[][99], int n)
 {
 	int i=0,j=0,k=0;
 	for (i=0;i<n;i++)
